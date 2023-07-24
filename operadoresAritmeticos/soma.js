9ccf04672c2ae5a9bc9c3e9d28a927493f64a75d
juniooor <juniormodern@hotmail.com>
2023-07-24 20:41:02 -0300

    operadores

---
+++ operadoresAritmeticos/soma.js
@@ -0,0 +1,18 @@
+let primeiro = 10
+let segundo = 5
+
+soma = primeiro + segundo
+subtrair = primeiro - segundo
+multiplica = primeiro * segundo
+divide = primeiro / segundo
+
+
+// console.log(soma)
+// console.log(multiplica)
+// console.log(subtrair)
+// console.log(divide)
+
+
+// PRECEDENCIA
+
+console.log((3 + 4) * 2)
