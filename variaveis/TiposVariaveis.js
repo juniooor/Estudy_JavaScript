aaa9180ed20ada6afd0ab34eafbe3a86021f5ad7
juniooor <juniormodern@hotmail.com>
2023-07-24 20:23:45 -0300

    firts commit

---
+++ variaveis/TiposVariaveis.js
@@ -0,0 +1,9 @@
+let idade = 22
+let nome = 'junin'
+let estudar = true
+let altura = 1.73
+
+console.log(idade, typeof idade)
+console.log(nome, typeof nome)
+console.log(estudar, typeof estudar)
+console.log(altura, typeof altura)
