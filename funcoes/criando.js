8e2de328127abf7337978654168f99f044ec0c78
juniooor <juniormodern@hotmail.com>
2023-07-27 20:37:03 -0300

    functions

---
+++ funcoes/criando.js
@@ -0,0 +1,13 @@
+function saudacao(nome, curso='javascript'){
+    console.log(`bem vindo ${nome}, ao curso de ${curso}`)
+}
+
+saudacao('junior', 'python')
+
+
+function soma(a, b){
+    return a + b 
+}
+
+
+console.log(soma(5, 3))
